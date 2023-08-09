284014480387abc9237707ee77ce61373e40cd5b
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-08-09 00:35:27 -0300

    basic testing

--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -1,13 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { expect } = require('chai');
-const request = require('supertest');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Pokemons, conn } = require('../../src/db.js');
+const { Pokemon, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const pokemon = {
-  name: 'Pikachu',
+  name: 'nuevopokemon',
+  image: 'urlImagen',
+  hp: 10,
+  attack: 10,
+  defense:10,
+  speed: 10,
+  weight: 10,
+  height: 10,
+  types: ["fire", "flying"]
 };
 
 describe('Pokemon routes', () => {
@@ -15,12 +22,26 @@ describe('Pokemon routes', () => {
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Pokemons.sync({ force: false }) 
-    .then(() => Pokemons.create(pokemon)));
+  describe('POST /pokemons', () => {
+    it('sending a pokemon should get 200 and receive a verification', async () => {
+      const res = await agent.post('/pokemons').send(pokemon)
+      expect(res.status).to.equal(200)
+      expect(res.body.msg).to.equal(`Pokemon ${pokemon.name} created`)
+    })
+    
+  })
   describe('GET /pokemons', () => {
-    it('should get 200', () =>
-      agent.get('/pokemons').expect(200)
-    );
-  }); 
+    it('should get 200', async () => {
+      const res = await agent.get('/pokemons')
+      expect(res.status).to.equal(200)
+    }).timeout(0)
+  });
+  
+  describe('GET /pokemons?name="pokemon"', () => {
+    it('should get 200', async () => {
+      const res = await agent.get(`/pokemons?name=${pokemon.name}`)
+      expect(res.status).to.equal(200)
+    })
+  })
+  
 });
- 
