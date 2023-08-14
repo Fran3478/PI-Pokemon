52f44998de49cdb901427be84c670bbdf59830f4
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-08-14 19:38:22 -0300

    add route create-pokemon to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home/Home'
 import LandingPage from './components/LandingPage/LandingPage'
 import Detail from './components/Detail/Detail'
+import Form from './components/Form/Form'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<LandingPage/>}/>
         <Route path={'/pokemons'} element={<Home/>}/>
         <Route path={'/detail/:id'} element={<Detail/>}/>
+        <Route path={'/create-pokemon'} element={<Form/>}/>
       </Routes>
       
     </div>
