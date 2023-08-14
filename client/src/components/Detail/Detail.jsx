92508fa401dcfd12769bceff581532a72ee5439c
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-08-13 23:15:50 -0300

    component Detail

---
+++ client/src/components/Detail/Detail.jsx
@@ -0,0 +1,47 @@
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { getById } from "../../redux/actions/actions";
+
+function Detail() {
+    const {id} = useParams()
+    const dispatch = useDispatch()
+    const pokemon = useSelector((state) => state.pokemon)
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        dispatch(getById(id))
+        .then(() => {
+            setLoading(false)
+        })
+    }, [dispatch])
+
+    return (
+        <div>
+            {
+                loading ? <p>Loading...</p> :
+                <div>
+                    <div>
+                        {pokemon.image ? <img src={pokemon.image} alt={pokemon.name}/> : null}
+                    </div>
+                    <div>
+                        <h1>{pokemon.name}</h1>
+                        <div>
+                            <p>HP: {pokemon.hp}</p>
+                            <p>Attack: {pokemon.attack}</p>
+                            <p>Defense: {pokemon.defense}</p>
+                            {pokemon.speed ? <p>Speed: {pokemon.speed}</p> : null}
+                            {pokemon.height ? <p>Height: {pokemon.height}</p> : null}
+                            {pokemon.weight ? <p>Weight: {pokemon.weight}</p> : null}
+                            <p>
+                                Types: {pokemon.types.map(type => <span key={type}>{type} </span>)}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+            }
+        </div>
+    )
+}
+
+export default Detail
